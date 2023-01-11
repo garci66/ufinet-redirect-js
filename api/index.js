3fc4cae3124d104cbca703ac321f0147e49f80b9
garci66 <garci66@gmail.com>
2023-01-11 18:21:42 -0300

    support ro and rw redirects

--- api/index.js
+++ api/index.js
@@ -25,10 +25,15 @@ const apiSytext= {
   }
 }
 
-function parseMultipleResults(sytexResults){
+function parseMultipleResults(sytexResults,ro){
   var retVal=""
   for (var thisForm of sytexResults.results){
-    retVal+=`<li><a href='${SYTEX_BASE_URL}/d/f/${thisForm.uuid}'>${thisForm.network_element.name} - ${thisForm.status.name}</a></li>`
+    if (ro){
+      retVal+=`<li><a href='${SYTEX_BASE_URL}/d/f/${thisForm.uuid}'>${thisForm.network_element.name} - ${thisForm.status.name}</a></li>`
+    } else {
+      retVal+=`<li><a href='${SYTEX_BASE_URL}/o/${thisForm.organization.id}${thisForm._url_display}'>${thisForm.network_element.name} - ${thisForm.status.name}</a></li>`
+    }
+    
   }
   return retVal
 }
@@ -42,6 +47,7 @@ export default async function (req) {
   
   const myTemplateType = myUrl.get('templateid');
   const myName = myUrl.get('nename');
+  const myRo = myUrl.get('ro');
 
   //console.log("template: " + myTemplateType + "\n name: " + myName)
 
@@ -49,14 +55,18 @@ export default async function (req) {
     const sytextResponse = await apiSytext.get(myTemplateType,myName)
 
     if (sytextResponse===null){
-      myResponse = new Response(`Hello, from ${req.url} - failed query`)
+      myResponse = new Response(`Hello, from ${req.url} - failed query`,{'status':408})
     }
     else if (sytextResponse.count==0){
-      myResponse = new Response(`Hello, from ${req.url} - No results found`)
+      myResponse = new Response(`Hello, from ${req.url} - No results found`, {'status':404})
     }
     else if (sytextResponse.count==1){
       if (sytextResponse.results[0]._url_display){
-        myResponse = Response.redirect(SYTEX_BASE_URL+"/d/f/" + sytextResponse.results[0].uuid)
+        if (!myRo){
+          myResponse = Response.redirect(SYTEX_BASE_URL+"/o/"+sytextResponse.results[0].organization.id + sytextResponse.results[0]._url_display)
+        } else {
+          myResponse = Response.redirect(SYTEX_BASE_URL+"/d/f/" + sytextResponse.results[0].uuid)
+        }
       }
       else {
         myResponse = new Response(`Hello, from ${req.url} - missing UUID`)
@@ -65,10 +75,10 @@ export default async function (req) {
     else{
       const myHeaders=new Headers()
       myHeaders.append('Content-Type', 'text/html');
-      myResponse= new Response(`Hello, from ${req.url} - possible multiple answers\nresponse:\n${parseMultipleResults(sytextResponse)}`, {headers:myHeaders});
+      myResponse= new Response(`Multiples posibles opciones\nElija:\n${parseMultipleResults(sytextResponse,myRo)}`, {headers:myHeaders});
     }
   } else {
-    myResponse = new Response(`Hello, from ${req.url} - invalid query parameters`)
+    myResponse = new Response(`Hello, from ${req.url} - invalid query parameters`,{'status':400})
   }
 
   return myResponse
